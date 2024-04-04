2158c7390b52f9591453719d69c461a2250540df
Mark Mwaura <mark.mwaura@lctafrica.net>
2024-04-04 22:38:00 +0300

    initial commit

---
+++ src/main/kotlin/com/djamo/IntegrationApi/IntegrationApiApplication.kt
@@ -0,0 +1,11 @@
+package com.djamo.IntegrationApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegrationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntegrationApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/djamo/IntegrationApi/IntegrationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.djamo.IntegrationApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntegrationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
